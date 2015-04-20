6a509bfaaa856e8eb1482b1d56de42d7d2f61c9f
jamallharris <jamallharris@absifs.com>
2015-04-20 12:35:33 -0500

    updated gh-pages to dist

---
+++ .tmp/concat/scripts/scripts.js
@@ -0,0 +1,71 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name cfsApp
+ * @description
+ * # cfsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('cfsApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(["$routeProvider", function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
+
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cfsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the cfsApp
+ */
+angular.module('cfsApp')
+  .controller('MainCtrl', ["$scope", function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  }]);
+
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name cfsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the cfsApp
+ */
+angular.module('cfsApp')
+  .controller('AboutCtrl', ["$scope", function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  }]);
